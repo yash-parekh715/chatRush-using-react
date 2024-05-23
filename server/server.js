b47cb4d1af62999a780c5b2362d88c59730dff36
yash-parekh715 <parekhyash712005@gmail.com>
2024-05-24 00:52:39 +0530

    fixing some cors bugs

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat--rush.vercel.app/",
+    origin: "https://chat--rush.vercel.app",
     methods: ["GET", "POST"],
   },
 });
